86d754e1061bf3354a8f5e7ec9dda6b817b7b17a
XCDesigner <sf-bvc@163.com>
2023-08-27 10:00:15 +0800

    NEW: Function implement

--- src/TcpProcessor/V30Commands.py
+++ src/TcpProcessor/V30Commands.py
@@ -70,6 +70,17 @@ class Commands:
     self.__SCMD_CTRL_LASER_ENABLE  = 0x0E
     self.__SCMD_CTRL_LASER_FOCUS   = 0x0F
     self.__SCMD_CTRL_GET_LASER_PERCENT   = 0x10
+
+    self.__CMD_REMOTE_PRINT = 0x0B
+    self.__SCMD_REMOTE_START = 0x01
+    self.__SCMD_REMOTE_STOP = 0x02
+    self.__SCMD_REMOTE_PAUSE = 0x03
+    self.__SCMD_REMOTE_RESUME = 0x04
+
+    self.__SCR_STATE_IDLE = 0
+    self.__SCR_STATE_PRINTING = 1
+    self.__SCR_STATE_PAUSED = 2
+    self.__SCR_STATE_MOTOR_BUSY = 3
     
     self.machine_status = MachineStatus()
     self.connect_id = 0
@@ -117,4 +128,106 @@ class Commands:
     retval += int.to_bytes(self.connect_id, 1, byteorder='little', signed=True)
     retval += int.to_bytes(target, 1, byteorder='little', signed=True)
     retval += int.to_bytes(temp, 2, byteorder='little', signed=True)
+    return retval
+
+  def req_home(self):
+    retval = b''
+    retval += int.to_bytes(self.__CMD_CONTROL, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(self.__SCMD_CTRL_HOME, 1, byteorder='little', signed=True)
+    retval += b'\x00'
+    retval += int.to_bytes(self.connect_id, 1, byteorder='little', signed=True)
+    return retval
+  
+  def req_home_axis(self, axis):
+    retval = b''
+    retval += int.to_bytes(self.__CMD_CONTROL, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(self.__SCMD_CTRL_HOME, 1, byteorder='little', signed=True)
+    retval += b'\x00'
+    retval += int.to_bytes(self.connect_id, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(axis, 1, byteorder='little', signed=True)
+    return retval
+  
+  def req_fan(self, fan, speed):
+    retval = b''
+    retval += int.to_bytes(self.__CMD_CONTROL, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(self.__SCMD_CTRL_FAN, 1, byteorder='little', signed=True)
+    retval += b'\x00'
+    retval += int.to_bytes(self.connect_id, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(fan, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(speed, 1, byteorder='little', signed=True)
+    return retval
+  
+  def req_move_axis(self, axis, distance):
+    distance_by_1000 = int(distance * 1000)
+    retval = b''
+    retval += int.to_bytes(self.__CMD_CONTROL, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(self.__SCMD_CTRL_MOVE, 1, byteorder='little', signed=True)
+    retval += b'\x00'
+    retval += int.to_bytes(self.connect_id, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(axis, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(distance_by_1000, 4, byteorder='little', signed=True)
+    return retval
+  
+  def req_extrude(self, nozzle, distance):
+    distance_by_1000 = int(abs(distance) * 1000)
+    retval = b''
+    retval += int.to_bytes(self.__CMD_CONTROL, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(self.__SCMD_CTRL_MOVE, 1, byteorder='little', signed=True)
+    retval += b'\x00'
+    retval += int.to_bytes(self.connect_id, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(nozzle, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(distance_by_1000, 4, byteorder='little', signed=False)
+    return retval
+  
+  def req_retract(self, nozzle, distance):
+    distance_by_1000 = int(abs(distance) * 1000)
+    retval = b''
+    retval += int.to_bytes(self.__CMD_CONTROL, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(self.__SCMD_CTRL_MOVE, 1, byteorder='little', signed=True)
+    retval += b'\x00'
+    retval += int.to_bytes(self.connect_id, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(nozzle, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(distance_by_1000, 4, byteorder='little', signed=False)
+    return retval
+  
+  def req_unlock_stepper(self):
+    retval = b''
+    retval += int.to_bytes(self.__CMD_CONTROL, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(self.__SCMD_CTRL_STOP_STEPPER, 1, byteorder='little', signed=True)
+    retval += b'\x00'
+    retval += int.to_bytes(self.connect_id, 1, byteorder='little', signed=True)
+    return retval
+  
+  def req_remote_print(self, file_name):
+    retval = b''
+    retval += int.to_bytes(self.__CMD_REMOTE_PRINT, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(self.__SCMD_REMOTE_START, 1, byteorder='little', signed=True)
+    retval += b'\x00'
+    retval += int.to_bytes(self.connect_id, 1, byteorder='little', signed=True)
+    retval += file_name.encode('utf-8')
+    retval += b'\x00'
+    return retval
+  
+  def req_remote_pause(self):
+    retval = b''
+    retval += int.to_bytes(self.__CMD_REMOTE_PRINT, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(self.__SCMD_REMOTE_PAUSE, 1, byteorder='little', signed=True)
+    retval += b'\x00'
+    retval += int.to_bytes(self.connect_id, 1, byteorder='little', signed=True)
+    return retval
+  
+  def req_remote_resume(self):
+    retval = b''
+    retval += int.to_bytes(self.__CMD_REMOTE_PRINT, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(self.__SCMD_REMOTE_RESUME, 1, byteorder='little', signed=True)
+    retval += b'\x00'
+    retval += int.to_bytes(self.connect_id, 1, byteorder='little', signed=True)
+    return retval
+  
+  def req_remote_stop(self):
+    retval = b''
+    retval += int.to_bytes(self.__CMD_REMOTE_PRINT, 1, byteorder='little', signed=True)
+    retval += int.to_bytes(self.__SCMD_REMOTE_STOP, 1, byteorder='little', signed=True)
+    retval += b'\x00'
+    retval += int.to_bytes(self.connect_id, 1, byteorder='little', signed=True)
     return retval
